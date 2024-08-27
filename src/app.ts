1b2b968e3e0dc86436a9ae9d16381bedc0b842fb
Gullian Mardones <g.mardones@outlook.com>
2024-08-26 23:30:56 -0400

    deploy

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,6 @@ const app = async () => {
   /**
    * PAGINA ESTATICA
    */
-  app.use(express.static(path.join(__dirname, "public", "index.html")))
 
   /**
    * INICIO DE DB
@@ -27,7 +26,7 @@ const app = async () => {
   app.use(bodyParser.json())
 
   //ROUTES
-  app.get('/', (req, res) => res.sendFile(path.join(__dirname, "..", 'public', "index.html")));
+  app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', "index.html")));
   app.use(mainRouter)
 
   app.listen(config.PORT, () => {
