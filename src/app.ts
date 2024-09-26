c2018537cdcf145500df2035d50f04b271e31b59
Gullian Mardones <g.mardones@outlook.com>
2024-09-25 23:57:29 -0300

    mod

--- src/app.ts
+++ src/app.ts
@@ -26,7 +26,7 @@ const app = async () => {
   app.use(bodyParser.json())
 
   //ROUTES
-  app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', "index.html")));
+  app.get('/', (req, res) => res.sendFile(path.join('src', 'public', "index.html")));
   app.use(mainRouter)
 
   app.listen(config.PORT, () => {
